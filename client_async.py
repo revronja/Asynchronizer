39171aa53c33001aa274305bc1a37f5959149830
revronja <37750403+revronja@users.noreply.github.com>
2019-10-27 17:05:45 -0500

    Update client_async.py

--- client_async.py
+++ client_async.py
@@ -1,40 +1,66 @@
 import asyncio
 import aiohttp
+import time
 
-""""
-10-27-19 sample responses
-making request to https://www.virustotal.com/vtapi/v2/url/scanhttps://www.google.com
-{"permalink": "https://www.virustotal.com/url/d0e196a0c25d35dd0a84593cbae0f38333aa58529936444ea26453eab28dfc86/analysis/1572210301/", 
-"resource": "https://www.google.com/", "url": "https://www.google.com/", "response_code": 1, "scan_date": "2019-10-27 21:15:52", "scan_id": "d0e196a0c25d35dd0a84593cbae0f38333aa58529936444ea26453eab28dfc86-1572210301", 
-"verbose_msg": "Scan request successfully queued, come back later for the report"}
-making request to https://www.virustotal.com/vtapi/v2/url/scanhttps://www.apple.com
-{"permalink": "https://www.virustotal.com/url/9e49cf4943550ed15085ca1e47265b880925c7a5f482b49af10662b0639b800c/analysis/1572210953/", 
-"resource": "https://www.apple.com/", "url": "https://www.apple.com/", "response_code": 1, "scan_date": "2019-10-27 21:15:53", "scan_id": "9e49cf4943550ed15085ca1e47265b880925c7a5f482b49af10662b0639b800c-1572210953", 
-"verbose_msg": "Scan request successfully queued, come back later for the report"}
-""""
 
-class Scanner:
+class URLScanner:
     def __init__(self):
        self.API_KEY = "key"
        self.BASE_URL = "https://www.virustotal.com/vtapi/v2/"
        self.SCAN_EP = "url/scan"
+       self.REPORT_EP = "url/report"
+
+    async def get_report(self, urls):
+        """
+        async function, an attribute of URLScanner class - response is ClientReponse type and is dict 
+        retrieves a report from VT and if not exist makes call to submit func to scan it
+        """
+        for url in urls:
+            print(f"making request to {self.BASE_URL}{self.REPORT_EP}{url}")
+            params = {"resource": url, "apikey": self.API_KEY}
+            async with aiohttp.ClientSession(connector=aiohttp.TCPConnector(ssl=False)) as session:
+                async with session.get(self.BASE_URL+'url/report', params=params ) as resp:
+                    if resp.status == 200:
+                        r = await resp.json()
+                        print(r)
+                        if r["response_code"] == 1:
+                            print("existing report found. "+str(r["response_code"]))
+                        elif if r["response_code"] == -2:
+                            print("scan queued come back later! "+str(r["response_code"]))
+                        else:
+                            make_request(url)
+                    elif resp.status == 204:
+                        print("rate limited! waiting.")
+                        time.sleep(60)
+
+                    else:
+                        print(resp.status)
 
     async def make_request(self, urls):
+        """
+        async function, an attribute of URLScanner class - response is ClientReponse type and is dict 
+        subimts a URL to VT 
+        """
         for url in urls:
             print(f"making request to {self.BASE_URL}{self.SCAN_EP}{url}")
             params = {"url": url, "apikey": self.API_KEY}
             async with aiohttp.ClientSession(connector=aiohttp.TCPConnector(ssl=False)) as session:
                 async with session.post(self.BASE_URL+'url/scan', params=params ) as resp:
                     if resp.status == 200:
-                        r = await resp.read()
-                        data = r.decode("utf-8")
-                        print(data)
+                        r = await resp.json()
+                        print(r)
+                
+                    elif resp.status == 204:
+                        print("rate limited! waiting.")
+                        time.sleep(60)
+
                     else:
                         print(resp.status)
+    
 
 
 if __name__ == "__main__":
     urls = ["https://www.google.com","https://www.apple.com"]
     loop = asyncio.get_event_loop()
-    scanner = Scanner()
-    loop.run_until_complete(scanner.make_request(urls))
+    scanner = URLScanner()
+    loop.run_until_complete(scanner.get_report(urls))
